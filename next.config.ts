7592a907edbad9841b09f9a4589c0f0a7d840122
boseung2 <jungbs3726@naver.com>
2026-01-08 02:02:32 +0900

    Setting: add deploy.yml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // 정적 배포를 위한 설정
+  images: {
+    unoptimized: true, // GitHub Pages에서 이미지를 최적화할 수 없으므로 필수
+  },
 };
 
 export default nextConfig;
